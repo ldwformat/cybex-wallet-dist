fcad4afec672d70e3da7e59864b642727755ffef
ldwformat <ldwformat@gmail.com>
2018-07-03 23:32:22 +0800

    fix wrong module

--- 24-ef090f2.js
+++
@@ -1,7 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{1995:
-/*!***************************************!*\
-  !*** ./assets/stylesheets/patch.scss ***!
-  \***************************************/
-/*! no static exports found */
-/*! all exports used */
-/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(n,w,o){}}]);
