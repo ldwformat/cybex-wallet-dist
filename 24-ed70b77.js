b7039277c4067f1282b65c2d66ce7d40bc2a6767
ldwformat <ldwformat@gmail.com>
2018-07-10 17:44:37 +0800

    Fix accountName input issue

---
+++ 24-ed70b77.js
@@ -0,0 +1,7 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{2023:
+/*!***************************************!*\
+  !*** ./assets/stylesheets/patch.scss ***!
+  \***************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(n,w,o){}}]);
