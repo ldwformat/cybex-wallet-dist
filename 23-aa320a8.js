a3710d766aa168ad0aef0b6e39aaf5a17562a934
ldwformat <ldwformat@gmail.com>
2018-07-12 17:08:18 +0800

    remove eth modal

--- 23-aa320a8.js
+++
@@ -1,7 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{1992:
-/*!***************************************!*\
-  !*** ./assets/stylesheets/patch.scss ***!
-  \***************************************/
-/*! no static exports found */
-/*! all exports used */
-/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(n,w,o){}}]);
