58e072c270fa0f343438b615cee17995f3695040
ldwformat <ldwformat@gmail.com>
2018-07-29 23:22:36 +0800

    Fix missing translate route

--- de8b2abea18fa280041c.worker.js
+++ de8b2abea18fa280041c.worker.js
@@ -929,7 +929,7 @@ var n=r(/*! base64-js */299),i=r(/*! ieee754 */298),o=r(/*! isarray */142);funct
 /*! ModuleConcatenation bailout: Cannot concat with ./cybex/cybexjs/ecc/src/PublicKey.js (<- Module uses injected variables (Buffer)) */
 /*! ModuleConcatenation bailout: Cannot concat with ./cybex/cybexjs/ecc/src/address.js (<- Module uses injected variables (Buffer)) */
 /*! ModuleConcatenation bailout: Cannot concat with ./cybex/cybexjs/ecc/src/aes.js (<- Module uses injected variables (Buffer)) */
-/*! ModuleConcatenation bailout: Cannot concat with ./cybex/cybexjs/ecc/src/hash.js because of ./cybex/cybexjs/ecc/src/address.js */
+/*! ModuleConcatenation bailout: Cannot concat with ./cybex/cybexjs/ecc/src/hash.js because of ./cybex/cybexjs/ecc/src/KeyUtils.js */
 /*! ModuleConcatenation bailout: Cannot concat with ./cybex/cybexjs/ecc/src/signature.js (<- Module uses injected variables (Buffer)) */
 /*! ModuleConcatenation bailout: Cannot concat with ./cybex/cybexjs/serializer/index.js because of ./cybex/cybexjs/chain/src/TransactionBuilder.js */
 /*! ModuleConcatenation bailout: Cannot concat with ./cybex/cybexjs/serializer/src/FastParser.js (<- Module uses injected variables (Buffer)) */
